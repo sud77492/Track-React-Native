a896e94c9acbf62536e12230121410ba387e9f2a
sud7749 <sameersharma078626@gmail.com>
2020-10-05 12:53:45 +0800

    first commit

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
+import { Context as AuthContext } from "../context/AuthContext";
+
+const SigninScreen = () => {
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
+  return (
+    <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign In to your Account"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        text="Dont have an account? Sign up instead"
+        routeName="Signup"
+      />
+    </View>
+  );
+};
+
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
+
+export default SigninScreen;
